d8ac8c5c8b9b07e123509368410fed3bbaffeca9
Chanathip Suprapakron <neetchanathip@gmail.com>
2017-08-28 09:22:51 +0700

    add ant.design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import {DatePicker} from 'antd';
+import {LocaleProvider} from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to App</h2>
+            <LocaleProvider locale={enUS}>
+                <div className="App">
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h2 className="aa">Welcome to App test</h2>
+                    </div>
+                    <p className="App-intro">
+                        Hello world!
+                    </p>
+                    <DatePicker/>
                 </div>
-                <p className="App-intro">
-                    Hello world! test aa
-                </p>
-            </div>
+            </LocaleProvider>
         );
     }
 }
