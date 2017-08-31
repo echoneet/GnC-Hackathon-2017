97e95a217f27bf9a323d470dde6765b03227a8ea
Thirajet <siyamike@gmail.com>
2017-08-31 11:04:17 +0700

    insert image base 64 on mongodb

--- service-uploadPayment/routes/index.js
+++ service-uploadPayment/routes/index.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({dest:'publice/images/'})
+var mongjs = require('mongojs');
+var db = mongjs('dev.iris.echoneet.space/Hackathon2017',['SlipPayment'])
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Upload Payment' });
 });
 
 router.post('/uploadPayment',function(req, res, next) {
-    console.log(req.body.img);
-    res.send("success");
+    //console.log(req.body.img);
+    db.SlipPayment.insert({"image":req.body.img},function (err,doc) {
+        if(err){
+            res.send("Error upload to db");
+        }
+        else {
+            res.send("success");
+        }
+    });
 });
 
 module.exports = router;
