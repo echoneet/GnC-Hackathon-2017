e532f2b25074514f57daa8646bfc05cb341dbe5f
Chanathip Suprapakron <neetchanathip@gmail.com>
2017-09-02 10:05:04 +0700

    add web service for register with google plus

--- service-register-with-google-plus/app.js
+++ service-register-with-google-plus/app.js
@@ -4,20 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongojs = require('mongojs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
 var passport = require('passport');
-var Strategy = require('passport-facebook').Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-var db = mongojs('dev.iris.echoneet.space/registerWithFacebookService', ["userdata"]);
-
-passport.use(new Strategy({
-        clientID: '128532184445160',
-        clientSecret: 'd36d0221f55e198b49c0c076eb764072',
-        callbackURL: 'http://localhost:8095/login/facebook/'
+passport.use(new GoogleStrategy({
+        clientID: '44383358345-gctfh0s007us3ogsl42qehfgk71vqhet.apps.googleusercontent.com',
+        clientSecret: 'gqPbtDaLRnukoQB1iL-BdRVW',
+        callbackURL: "http://localhost:8097/auth/google/"
     },
     function (accessToken, refreshToken, profile, cb) {
         return cb(null, profile);
@@ -41,9 +37,10 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -53,36 +50,28 @@ app.use('/users', users);
 app.use('/login/fail', function (req, res, next) {
     res.render('failLogin');
 });
-
-app.get('/login/facebook',
-    passport.authenticate('facebook', {failureRedirect: '/login/fail'}),
-    function (req, res) {
-        db.userdata.findOne({id: req.user.id}, function (err, docs) {
-            console.log(docs);
-            if (docs === null) {
-                db.userdata.insert(req.user);
-            }
-        });
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'],failureRedirect: '/login/fail' }),
+    function(req, res) {
         res.send(req.user)
     });
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
-
 
 module.exports = app;
