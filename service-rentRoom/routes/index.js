848f436021ca57ef8bdd5b6b66fa6a4281eb6f5a
Chanathip Suprapakron <neetchanathip@gmail.com>
2017-09-02 16:13:32 +0700

    fix for docker

--- service-rentRoom/routes/index.js
+++ service-rentRoom/routes/index.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var axios = require('axios');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RentRoom' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'RentRoom'});
 });
 
-router.post('/RentRoom', function(req, res, next) {
-    axios.post('http://localhost:8096/RentRoomInManage',{
-        renter:req.body.renter,
-        id:req.body.id
+router.post('/RentRoom', function (req, res, next) {
+    axios.post('http://roommanage:8096/RentRoomInManage', {
+        renter: req.body.renter,
+        id: req.body.id
     })
         .then(function (response) {
             console.log(response.data)
